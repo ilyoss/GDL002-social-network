bb531df0585f447f08582a1d1f537d7e3594d9e8
ilyoss <ily.trevino@live.com>
2019-03-28 09:42:56 -0600

    Changed the use of history to htmls instead of js files

--- src/index.js
+++ src/index.js
@@ -1,43 +1,36 @@
+//Div in my main html file where I will push my content, depending on the route
 let contentDiv = document.getElementById('content');
-
+//I set up my routes for the webpage, and assign them my htmls
 let routes = {
-  '/': homepage,
-  '/index.html': homepage,
-  '/newsfeed': newsfeed,
+  '/': './templates/login.html',
+  '/index.html': './templates/login.html',
+  '/newsfeed': './templates/newsfeed.html',
 };
-
+//
 window.onpopstate = () => {
-  contentDiv.innerHTML = routes[window.location.pathname];
+  fetchContent(routes[window.location.pathname])
+  .then(html => contentDiv.innerHTML = html);
 }
-
+//Function that I use when the interaction on my DOM requires to change page
 let onNavItemClick = (pathName) => {
   window.history.pushState({}, pathName, window.location.origin + pathName);
-  contentDiv.innerHTML = routes[pathName];
+  //I fetch my html content and send it to the innerHTML
+  fetchContent(routes[window.location.pathname])
+  .then(html => contentDiv.innerHTML = html);
 }
+//My fetchContent function, which gets the data in my htmls and translates it to text, which I then can use
+const fetchContent = (url) => fetch(url)
+    .then(function(response) {
+        // When the page is loaded convert it to text
+        return response.text()
+    })
+    .then(function(html) {
+        //Returns my html section
+        return html;
+    })
+    .catch(function(err) {
+        console.log('Failed to fetch page: ', err);
+    });
 
-contentDiv.innerHTML = routes[window.location.pathname];
-
-// function loginPage() {
-//
-//   fetch('login.html')
-//       .then(function(response) {
-//           // When the page is loaded convert it to text
-//           return response.text()
-//       })
-//       .then(function(html) {
-//           // Initialize the DOM parser
-//           var parser = new DOMParser();
-//
-//           // Parse the text
-//           var doc = parser.parseFromString(html, "text/html");
-//
-//           // You can now even select part of that html as you would in the regular DOM
-//           // Example:
-//           // var docArticle = doc.querySelector('article').innerHTML;
-//
-//           console.log(doc);
-//       })
-//       .catch(function(err) {
-//           console.log('Failed to fetch page: ', err);
-//       });
-// }
+fetchContent(routes[window.location.pathname])
+.then(html => contentDiv.innerHTML = html);
