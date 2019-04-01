7883209919f5d3219f0223cbab9e7867b9fbf405
ilyoss <ily.trevino@live.com>
2019-04-01 15:06:52 -0600

    Solved dynamic eventlisteners

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ let contentDiv = document.getElementById('content');
 //I set up my routes for the webpage, and assign them my htmls
 let routes = {
   '/': './templates/login.html',
-  '/index.html': './templates/login.html',
   '/newsfeed': './templates/newsfeed.html',
+  '/profile': './templates/profile.html',
+  '/about': './templates/aboutus.html',
 };
 //
 window.onpopstate = () => {
@@ -16,7 +17,8 @@ let onNavItemClick = (pathName) => {
   window.history.pushState({}, pathName, window.location.origin + pathName);
   //I fetch my html content and send it to the innerHTML
   fetchContent(routes[window.location.pathname])
-  .then(html => contentDiv.innerHTML = html);
+  .then(html => contentDiv.innerHTML = html)
+  .then(() => addEventListeners());
 }
 //My fetchContent function, which gets the data in my htmls and translates it to text, which I then can use
 const fetchContent = (url) => fetch(url)
@@ -33,4 +35,18 @@ const fetchContent = (url) => fetch(url)
     });
 
 fetchContent(routes[window.location.pathname])
-.then(html => contentDiv.innerHTML = html);
+.then(html => contentDiv.innerHTML = html)
+.then(() => addEventListeners());
+
+//Function to add event listeners after loading my template
+function addEventListeners(){
+  const btnList = document.querySelectorAll(".jsBtn");
+
+  for (let i = 0; i < btnList.length; i++) {
+     btnList[i].addEventListener("click", function(event) {
+       // console.dir(event.target);
+       // console.dir(event.target.attributes.dataFirst.value);
+       socialNetwork[event.target.dataset.next](document.getElementById(event.target.attributes.dataFirst.value).value, document.getElementById(event.target.attributes.dataSecond.value).value);
+     });
+  }
+}
--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
-                      //BUTTONS
+//BUTTONS
 //All buttons
 const signInBtn = document.getElementById("signIn"); //Sign in!
 const signUpBtn = document.getElementById("signUpBtn"); //Sign up
 const logOutBtn = document.getElementById("logOut"); //Log Out
 
-                      //INPUTS
+//INPUTS
 //Login form
-const loginEmail = document.getElementById('loginEmail');
-const loginPassword = document.getElementById('loginPassword');
+// const loginEmail = document.getElementById('loginEmail');
+// const loginPassword = document.getElementById('loginPassword');
 //Registration form
 const registerName = document.getElementById('name');
 const registerLast = document.getElementById('lastname');
@@ -16,12 +16,12 @@ const registerEmail = document.getElementById('email');
 const registerPassword = document.getElementById('password');
 const cardElement = document.getElementById("card");
 
-                      //EVENT LISTENERS
-signInBtn.addEventListener("click", login);
-signUpBtn.addEventListener("click", register);
+//EVENT LISTENERS
+// signInBtn.addEventListener("click", login);
+// signUpBtn.addEventListener("click", register);
 // logOut.addEventListener("click", logout);
 
-                      //FUNCTIONS
+//FUNCTIONS
 
 //Function to flip "card", alternate between login or register visual
 function flip() {
@@ -30,20 +30,22 @@ function flip() {
 }
 
 //Function in which I login the user, as long as the user and password exists in the data base
-function login() {
+function login(email, password) {
+  //
+  // const email = document.getElementById('loginEmail').value;
+  // const password = document.getElementById('loginPassword').value;
 
-  let email = loginEmail.value;
-  let password = loginPassword.value;
-
-  firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    alert("Something went wrong :( " + errorMessage);
-    // ...
+  firebase.auth().signInWithEmailAndPassword(email, password)
+  .then(() =>
+    onNavItemClick('/newsfeed')
+  )
+  .catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  alert("Something went wrong :( " + errorMessage);
+  // ...
   });
-
-  onNavItemClick('/newsfeed')
 }
 
 function register(){
@@ -52,62 +54,59 @@ function register(){
   let userPassword = registerPassword.value;
   //Firebase function to create a new user in the registration area, with email and password
   firebase.auth().createUserWithEmailAndPassword(userEmail, userPassword).catch(function failure(error) {
-          var errorCode = error.code;
-          var errorMessage = error.message;
-          alert(errorCode + "- Oh no, something went wrong :( " + errorMessage);
-      });
-      //After the user is created I check that the user is signed in
-      firebase.auth().onAuthStateChanged(function(user) {
-        //If my new user is logged in (which is true, because after registration it logs in automatically)
-        if (user) {
-          //I save the uid in a variable
-          let uid = user.uid;
-          //I call my save data function and send the uid
-          saveData(uid);
-          //Change my screen to newsfeed page
-          loginScreen.style.display = 'none';
-          logoutScreen.style.display = 'block';
-          // ...
-        } else {
-          // User is signed out.
-          // ...
-        }
-      });
-
-      onNavItemClick('/newsfeed')
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    alert(errorCode + "- Oh no, something went wrong :( " + errorMessage);
+  });
+  //After the user is created I check that the user is signed in
+  firebase.auth().onAuthStateChanged(function(user) {
+    //If my new user is logged in (which is true, because after registration it logs in automatically)
+    if (user) {
+    //I save the uid in a variable
+    let uid = user.uid;
+      //I call my save data function and send the uid
+      saveData(uid);
+      onNavItemClick('/newsfeed');
+    // ...
+    } else {
+      // User is signed out.
+      // ...
+    }
+  });
 }
 
 //Escribir en la base de datos
 function saveData(uid){
-  let user = {
-    nombre: registerName.value,
-    apellido: registerLast.value,
-    ciudad: registerCity.value,
-    email: registerEmail.value,
-    password: registerPassword.value,
-  }
-  //Dentro de mi rama de usuarios, guardo el usuario con su uid
-  firebase.database().ref("users/" + uid)
-  .set(user);
-  clearInputs();
+let user = {
+nombre: registerName.value,
+apellido: registerLast.value,
+ciudad: registerCity.value,
+email: registerEmail.value,
+password: registerPassword.value,
+}
+//Dentro de mi rama de usuarios, guardo el usuario con su uid
+firebase.database().ref("users/" + uid)
+.set(user);
+clearInputs();
 }
 
 function clearInputs(){
-  registerName.value = "";
-  registerLast.value = "";
-  registerCity.value = "";
-  registerEmail.value = "";
-  registerPassword.value = "";
+registerName.value = "";
+registerLast.value = "";
+registerCity.value = "";
+registerEmail.value = "";
+registerPassword.value = "";
 }
 
 function logout() {
 
-  loginScreen.style.display = 'block';
-  logoutScreen.style.display = 'none';
-
-  firebase.auth().signOut().then(function() {
-    // Sign-out successful.
-  }).catch(function(error) {
-    // An error happened.
-  });
+firebase.auth().signOut().then(function() {
+// Sign-out successful.
+}).catch(function(error) {
+// An error happened.
+});
 }
+
+const socialNetwork = {
+  login: login,
+};
